47146e9be5464bf468c3219facdaab5be99fa1c1
dongyu.cai <458498007@qq.com>
2022-10-24 22:00:25 +0800

    Initial Commit

---
+++ WeChat/WeChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeChatApp.swift
+//  WeChat
+//
+//  Created by cai dongyu on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
