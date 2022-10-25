20120ede007f1d66675d454221752e6c31f4312d
dongyu.cai <458498007@qq.com>
2022-10-25 13:27:53 +0800

    feat: building the explorationview

---
+++ WeChat/Exploration/ExplorationView.swift
@@ -0,0 +1,33 @@
+//
+//  ExplorationView.swift
+//  WeChat
+//
+//  Created by cai dongyu on 2022/10/25.
+//
+
+import SwiftUI
+
+struct ExplorationView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink(destination: {
+                    TimelineView()
+                }, label: {
+                    Label("朋友圈", systemImage: "circle.circle.fill")
+                })
+                Label("游戏", systemImage: "gamecontroller.fill")
+                Label("小程序", systemImage: "circle.dashed")
+            }
+            .navigationTitle("发现")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct ExplorationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExplorationView()
+    }
+}
---
+++ WeChat/Timeline/View/TimelineView.swift
@@ -0,0 +1,20 @@
+//
+//  TimelineView.swift
+//  WeChat
+//
+//  Created by cai dongyu on 2022/10/25.
+//
+
+import SwiftUI
+
+struct TimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimelineView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimelineView()
+    }
+}
